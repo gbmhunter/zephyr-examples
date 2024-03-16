779ffe8ba2131d4867c843cc5bd0ef0fce156200
Geoffrey Hunter <gbmhunter@gmail.com>
2024-03-16 22:04:59 +1300

    Test with zbus.

--- apps/cpp-app-framework/src/main.cpp
+++ apps/cpp-app-framework/src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/zbus/zbus.h>
 
 #include "Led.h"
 #include "StateMachine.h"
@@ -19,6 +20,23 @@ void ledThreadFnAdapter(void *, void *, void *) {
     l_led->threadFn();
 }
 
+struct acc_msg {
+	int x;
+	int y;
+	int z;
+};
+
+ZBUS_CHAN_DEFINE(acc_data_chan,  /* Name */
+		 struct acc_msg, /* Message type */
+
+		 NULL,                                 /* Validator */
+		 NULL,                                 /* User data */
+		 ZBUS_OBSERVERS(bar_sub),     /* observers */
+		 ZBUS_MSG_INIT(.x = 0, .y = 0, .z = 0) /* Initial value */
+);
+
+ZBUS_SUBSCRIBER_DEFINE(bar_sub, 4);
+
 int main(void) {
 
     auto led = Led(ledThreadStack, LED_THREAD_STACK_SIZE_B, &ledThreadFnAdapter);
@@ -34,6 +52,18 @@ int main(void) {
 
     // Make the LED flash
     led.blink(5, 1000, 1000);
+
+    const struct zbus_channel *chan;
+
+	while (!zbus_sub_wait(&bar_sub, &chan, K_FOREVER)) {
+		struct acc_msg acc;
+
+		if (&acc_data_chan == chan) {
+			zbus_chan_read(&acc_data_chan, &acc, K_MSEC(500));
+
+			LOG_INF("From subscriber -> Acc x=%d, y=%d, z=%d", acc.x, acc.y, acc.z);
+		}
+	}
     
     LOG_DBG("Terminating thread\n");
     led.terminateThread();
